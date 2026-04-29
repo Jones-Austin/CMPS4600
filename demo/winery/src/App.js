829c5b3212ac622306e2d20dd7668de5302a74bc
Jones-Austin <amjones1@pointpark.edu>
2026-04-29 16:48:39 -0400

    Assignment 5

---
+++ demo/winery/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import Hola from './components/hola';
+import Welcome from './components/welcome';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Hola />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ demo/winery/src/components/hola.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+/*
+function Hola() {
+    return <h1>Hello World</h1>
+}
+*/
+
+
+const Hola = () => <h1>Wine World</h1>;
+
+
+export default Hola;
